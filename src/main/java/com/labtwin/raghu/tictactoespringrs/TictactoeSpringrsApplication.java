c466e851a2f23bd5059e157f98555125962f5eee
raghavendhar <raghavendhar.123@gmail.com>
2022-01-14 23:01:09 +0530

    Initial spring boot starter project.

---
+++ src/main/java/com/labtwin/raghu/tictactoespringrs/TictactoeSpringrsApplication.java
@@ -0,0 +1,13 @@
+package com.labtwin.raghu.tictactoespringrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeSpringrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeSpringrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labtwin/raghu/tictactoespringrs/TictactoeSpringrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labtwin.raghu.tictactoespringrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeSpringrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
